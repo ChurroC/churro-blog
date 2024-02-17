c91b5b52cbfdbef78c262f806d22f8e794c69959
ChurroC <charanvenkatchandran@gmail.com>
2024-02-16 23:48:30 -0600

    Didn't do much just worked on the cli from Josh Comau

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-gray-800 text-white p-4 text-center">
-            <p>© 2021 Robotics</p>
-        </footer>
-    );
-}
