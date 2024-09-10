e331b15fc595bc1ab9d36162871cbef1c82d4c85
ChurroC <66274903+ChurroC@users.noreply.github.com>
2024-09-10 16:43:50 +0000

    gonna delete all old stuff after this and replace with radix primitives

--- src/components/header/icons/darkModeIcon.tsx
+++ src/components/header/icons/darkModeIcon.tsx
@@ -11,61 +11,67 @@ import {
 import { useGetTheme, useSetTheme } from "@/util/contexts/theme";
 import { type Theme } from "@/util/contexts/theme";
 import { twMerge } from "tailwind-merge";
-import {Button, Menu, MenuItem, MenuTrigger, Popover} from 'react-aria-components';
-import {FocusScope} from 'react-aria';
-
+import { FocusScope } from "react-aria";
+import * as Popover from "@radix-ui/react-popover";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 // Client component since local storage
 export function DarkModeIcon() {
     const [theme, setTheme] = [useGetTheme(), useSetTheme()];
 
     return (
-        // <Dropdown>
-        //     <DropdownTrigger>
-        //         <HeaderIcon className="w-8">
-        //             {
-        //                 {
-        //                     light: <SunIcon className="h-5" />,
-        //                     dark: <MoonIcon className="h-5" />,
-        //                     system: <ComputerDesktopIcon className="h-5" />
-        //                 }[theme]
-        //             }
-        //         </HeaderIcon>
-        //     </DropdownTrigger>
-        //     <DropdownContent>
-        //         {(["light", "dark", "system"] as Theme[]).map(themeOption => {
-        //             return (
-        //                 <li
-        //                     key={themeOption}
-        //                     onClick={() => setTheme(themeOption)}
-        //                     className={twMerge(
-        //                         "capitalize text-neutral-400",
-        //                         theme === themeOption && "text-neutral-950"
-        //                     )}
-        //                 >
-        //                     {themeOption}
-        //                 </li>
-        //             );
-        //         })}
-        //     </DropdownContent>
-        // </Dropdown>
-        // <MenuTrigger>
-        //   <Button aria-label="Menu">☰</Button>
-        //   <Popover>
-        //     <Menu>
-        //       <MenuItem onAction={() => alert('open')}>Open</MenuItem>
-        //       <MenuItem onAction={() => alert('rename')}>Rename…</MenuItem>
-        //       <MenuItem onAction={() => alert('duplicate')}>Duplicate</MenuItem>
-        //       <MenuItem onAction={() => alert('share')}>Share…</MenuItem>
-        //       <MenuItem onAction={() => alert('delete')}>Delete…</MenuItem>
-        //     </Menu>
-        //   </Popover>
-        // </MenuTrigger>
-        
-        <>
-          <MenuTrigger>
+        <DropdownMenu.Root modal={false}>
+            <DropdownMenu.Trigger asChild>
+                <HeaderIcon className="w-8">
+                    {
+                        {
+                            light: <SunIcon className="h-5" />,
+                            dark: <MoonIcon className="h-5" />,
+                            system: <ComputerDesktopIcon className="h-5" />
+                        }[theme]
+                    }
+                </HeaderIcon>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal></DropdownMenu.Portal>
+        </DropdownMenu.Root>
+    );
+}
+
+/*
+
+        <Dropdown>
+            <DropdownTrigger>
+                <HeaderIcon className="w-8">
+                    {
+                        {
+                            light: <SunIcon className="h-5" />,
+                            dark: <MoonIcon className="h-5" />,
+                            system: <ComputerDesktopIcon className="h-5" />
+                        }[theme]
+                    }
+                </HeaderIcon>
+            </DropdownTrigger>
+            <DropdownContent>
+                {(["light", "dark", "system"] as Theme[]).map(themeOption => {
+                    return (
+                        <li
+                            key={themeOption}
+                            onClick={() => setTheme(themeOption)}
+                            className={twMerge(
+                                "capitalize text-neutral-400",
+                                theme === themeOption && "text-neutral-950"
+                            )}
+                        >
+                            {themeOption}
+                        </li>
+                    );
+                })}
+            </DropdownContent>
+        </Dropdown>
+        <MenuTrigger>
           <Button aria-label="Menu">☰</Button>
-          <Popover isNonModal>
+          <Popover>
             <Menu>
               <MenuItem onAction={() => alert('open')}>Open</MenuItem>
               <MenuItem onAction={() => alert('rename')}>Rename…</MenuItem>
@@ -75,6 +81,4 @@ export function DarkModeIcon() {
             </Menu>
           </Popover>
         </MenuTrigger>
-      </>
-    );
-}
+*/
