7e22c7a51fbfe0b7bddda7bdc4e18524bc09bd4a
ChurroC <66274903+ChurroC@users.noreply.github.com>
2024-09-10 16:59:03 +0000

    got something going I feel sick

--- src/components/header/icons/darkModeIcon.tsx
+++ src/components/header/icons/darkModeIcon.tsx
@@ -3,25 +3,17 @@
 import { HeaderIcon } from "@/components/header/icons/headerIcon";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import { ComputerDesktopIcon } from "@heroicons/react/20/solid";
-import {
-    Dropdown,
-    DropdownContent,
-    DropdownTrigger
-} from "@/components/dropdown";
-import { useGetTheme, useSetTheme } from "@/util/contexts/theme";
-import { type Theme } from "@/util/contexts/theme";
 import { twMerge } from "tailwind-merge";
-import { FocusScope } from "react-aria";
-import * as Popover from "@radix-ui/react-popover";
+import { useTheme, type Theme } from "next-server-theme";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 // Client component since local storage
 export function DarkModeIcon() {
-    const [theme, setTheme] = [useGetTheme(), useSetTheme()];
+    const [theme, setTheme] = useTheme();
 
     return (
         <DropdownMenu.Root modal={false}>
-            <DropdownMenu.Trigger asChild>
+            <DropdownMenu.Trigger>
                 <HeaderIcon className="w-8">
                     {
                         {
@@ -32,53 +24,26 @@ export function DarkModeIcon() {
                     }
                 </HeaderIcon>
             </DropdownMenu.Trigger>
-
-            <DropdownMenu.Portal></DropdownMenu.Portal>
+            <DropdownMenu.Portal>
+                <DropdownMenu.Content className="flex w-fit flex-col rounded-md border border-neutral-200 bg-white p-1 shadow-md">
+                    {(["light", "dark", "system"] as Theme[]).map(
+                        themeOption => {
+                            return (
+                                <DropdownMenu.Item
+                                    key={themeOption}
+                                    onClick={() => setTheme(themeOption)}
+                                    className={`flex items-center justify-between rounded-sm px-2 py-1.5 text-left text-sm capitalize text-neutral-400 hover:bg-zinc-100 ${
+                                        theme === themeOption &&
+                                        "text-neutral-950"
+                                    }`}
+                                >
+                                    {themeOption}
+                                </DropdownMenu.Item>
+                            );
+                        }
+                    )}
+                </DropdownMenu.Content>
+            </DropdownMenu.Portal>
         </DropdownMenu.Root>
     );
 }
-
-/*
-
-        <Dropdown>
-            <DropdownTrigger>
-                <HeaderIcon className="w-8">
-                    {
-                        {
-                            light: <SunIcon className="h-5" />,
-                            dark: <MoonIcon className="h-5" />,
-                            system: <ComputerDesktopIcon className="h-5" />
-                        }[theme]
-                    }
-                </HeaderIcon>
-            </DropdownTrigger>
-            <DropdownContent>
-                {(["light", "dark", "system"] as Theme[]).map(themeOption => {
-                    return (
-                        <li
-                            key={themeOption}
-                            onClick={() => setTheme(themeOption)}
-                            className={twMerge(
-                                "capitalize text-neutral-400",
-                                theme === themeOption && "text-neutral-950"
-                            )}
-                        >
-                            {themeOption}
-                        </li>
-                    );
-                })}
-            </DropdownContent>
-        </Dropdown>
-        <MenuTrigger>
-          <Button aria-label="Menu">☰</Button>
-          <Popover>
-            <Menu>
-              <MenuItem onAction={() => alert('open')}>Open</MenuItem>
-              <MenuItem onAction={() => alert('rename')}>Rename…</MenuItem>
-              <MenuItem onAction={() => alert('duplicate')}>Duplicate</MenuItem>
-              <MenuItem onAction={() => alert('share')}>Share…</MenuItem>
-              <MenuItem onAction={() => alert('delete')}>Delete…</MenuItem>
-            </Menu>
-          </Popover>
-        </MenuTrigger>
-*/
