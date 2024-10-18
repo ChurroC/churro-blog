091ce9226333294182c81539e403ab7447b10ad5
ChurroC <66274903+ChurroC@users.noreply.github.com>
2024-10-18 16:59:35 +0000

    cookin?

--- testtt/src/App.tsx
+++ testtt/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
                             accessToken:
                                 "R3gUAad3z4rNZudzeVKArkqOA7kbdvE3UXeD1Gq38bDIhQQvHOo5K4GZ8GNU8GCp"
                         }}
-                        urn={"<your-model-urn>"}
+                        urn={"dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Y2h1cnJvYy1jb29sLWJ1Y2tldC1uYW1lL1JhdGNoZXQuZjNk"}
                     />
                 </div>
             </div>
